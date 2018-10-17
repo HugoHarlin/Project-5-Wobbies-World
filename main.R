9e8d27b6fd6e77bec192f96f3f1b6c8cb5d21fe6
HugoHarlin <36413511+HugoHarlin@users.noreply.github.com>
2018-10-17 12:31:49 +0200

    kody

--- main.R
+++ main.R
@@ -1,8 +1,9 @@
 rm(list = ls())
-setwd("/Users/KGirl/Documents/GitHub/Project-5-Wobbies-World/")
-#setwd("~/Teknisk Fysik/AI/Project-5-Wobbies-World")
+#setwd("/Users/KGirl/Documents/GitHub/Project-5-Wobbies-World/")
+setwd("~/Teknisk Fysik/AI/Project-5-Wobbies-World")
 library(WobbiesWorld)
+source("myFunction.R")
 source("makeOwnController.R")
-res = runGame(control = makeOwnController(maze), doPlot = T)
+res = runGame(control = myFunction(maze), doPlot = T)
 #runWW(makeController = makeRandomController)
 
--- myFunction.R
+++ myFunction.R
@@ -7,17 +7,46 @@ myFunction = function (maze){
       # nr moves per state: 5
       Q = rep(0,27*5*27)
       cont$Q = Q
+      cont$moves = c(2,4,5,6,8)
+      cont$alpha = 0.01
+    }
+    # wobbies and monsters node NR
+    nodeNr_wobbie = which(maze_$maze$x == maze_$wobbie[1] & maze_$maze$y == maze_$wobbie[2])
+    nodeNr_monster = which(maze_$maze$x == maze_$monster1[1] & maze_$maze$y == maze_$monster1[2])
+    
+    # Q-vals of the five possible moves in this state (5 element vector)
+    Q_vals = cont$Q[(nodeNr_monster-1)*27*5 + (nodeNr_wobbie-1)*5+ c(1,2,3,4,5)]
+    show("Q_vals")
+    show(Q_vals)
+    
+    # selecting the best move
+    moves = which(max(Q_vals) == Q_vals)
+    show("moves")
+    show(moves)
+    if(length(moves) > 1){
+      bestMove = sample(moves,1)
+    }
+    else{
+      bestMove = moves
     }
     
-    nodeNr = match(maxe_$wobbie[1],maze_$wobbie[,1]
-    which(maze_maze)
+    show("bestMove")
+    show(bestMove)
+    finalMove = cont$moves[bestMove]
     
-    return(list(move = sample(c(2, 4, 6, 8), 1), control = cont))
+    # sampling random move with probability alpha
+    if(runif(1) < cont$alpha){
+      finalMove = sample(cont$moves,1)
+    }
+    show("finalMove")
+    show(finalMove)
+    
+    return(list(move = finalMove, control = cont))
   }
-
+  
   update = function(cont, maze_) {
-    show("maze_")
-    show(maze_)
+    #show("maze_")
+    #show(maze_)
     #show("cont")
     #show(cont)
     show("----------------------------")
